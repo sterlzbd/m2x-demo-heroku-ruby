d3990c5645b3667dbad351d7db061d3211fa2d2a
John Mark Schofield <jms@schof.org>
2014-04-11 16:30:23 -0700

    Still trying to get heroku to sleep

--- loadreport.rb
+++ loadreport.rb
@@ -6,12 +6,13 @@ BPNAME = "loadreport-heroku"
 
 
 puts "Starting loadreport.rb run"
+sleep 30
+puts "after sleep"
 
 APIKEY = File.read(File.join(File.dirname(__FILE__), 'm2x_api_key.txt')).strip
 
 m2x = M2X.new(APIKEY)
 
-sleep 30
 
 # Test to see if our blueprint exists
 loadreport_blueprint_exists = false
