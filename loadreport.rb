6198214cb242692648b18fe49874340f862e34a4
Leandro López (inkel) <inkel.ar@gmail.com>
2014-08-12 15:09:47 -0300

    Update Ruby Heroku demo

--- loadreport.rb
+++ loadreport.rb
@@ -30,14 +30,25 @@ if not loadreport_blueprint_exists
     lr_blueprint = m2x.blueprints.create(name: BPNAME, visibility: "private", description: "Load Report")
 end
 
+# Create the streams if they don't exist
+m2x.feeds.update_stream(lr_blueprint["id"], "load_1m")
+m2x.feeds.update_stream(lr_blueprint["id"], "load_5m")
+m2x.feeds.update_stream(lr_blueprint["id"], "load_15m")
+
 # Get our load data from the system
 # Match `uptime` load averages output for both Linux and OSX
 UPTIME_RE = /(\d+\.\d+),? (\d+\.\d+),? (\d+\.\d+)$/
 load_1m, load_5m, load_15m = `uptime`.match(UPTIME_RE).captures
 
 # Write the different values into AT&T M2X
-m2x.feeds.update_stream(lr_blueprint["id"], "load_1m",  value: load_1m)
-m2x.feeds.update_stream(lr_blueprint["id"], "load_5m",  value: load_5m)
-m2x.feeds.update_stream(lr_blueprint["id"], "load_15m", value: load_15m)
+now = Time.now.iso8601
+
+values = {
+  load_1m:  [ { value: load_1m, at: now } ],
+  load_5m:  [ { value: load_5m, at: now } ],
+  load_15m: [ { value: load_15m, at: now } ]
+}
+
+m2x.feeds.post_multiple(lr_blueprint["id"], values)
 
 puts "Ending loadreport.rb run"
